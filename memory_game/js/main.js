c937a3ee3fe40852da1aaa8ac9bdf4762cb0e853
Ivor King <Ivor King>
2018-02-18 14:03:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne, cardTwo);
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+console.log("user flipped " + cardsInPlay);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("user flipped " + cardsInPlay[1]);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("The same!");
+	}
+	else {
+		alert("sorry try again");
+	}
+}
