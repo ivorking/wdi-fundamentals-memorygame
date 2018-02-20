020896c5bcb416f7bdae7f320fb6e824674f4939
Ivor King <Ivor King>
2018-02-20 02:02:58 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ['queen','queen','king','king'];
+var cards = [
+	{
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "Queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 function checkForMatch() {
 //	if (cardsInPlay.length === 2) {
@@ -16,8 +37,10 @@ function flipCard(cardID) {
 //	console.log("user flipped " + cardsInPlay[0]);
 //	cardsInPlay.push(cardTwo);
 //	console.log("user flipped " + cardsInPlay[1]);
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 	checkForMatch();
 }
 
